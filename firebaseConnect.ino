265ce15735eaa801bcbb4d3fadeb85761c70f523
RizkyChandraWijaya <89894989+RizkyChandraWijaya@users.noreply.github.com>
2021-12-21 21:05:15 +0700

    Update firebaseConnect.ino

--- firebaseConnect.ino
+++ firebaseConnect.ino
@@ -23,33 +23,31 @@ FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
-
-unsigned long count = 0;
+int merah = 16;
+int kuning = 17;
 
 AfstandsSensor afstandssensor(23, 22);
 
 //globar variable servo
 static const int servoPin = 25; //printed G14 on the board
 Servo_ESP32 servo1;
+
 int angle = 25;
 int angleStep = 1;
 int angleMin = 20;
 int angleMax = 130;
 
 //globar var data firebase
-int jarak = 0;
+int tinggiAir = 0;
 int delayGate = 0;
 String statusGate = "open";
 
-int jarakSensor = 0;
-
+int tinggiSensor = 0;
 
 void openGate(){
    servo1.attach(servoPin);
     for(int angle = 130; angle >= angleMin; angle -=angleStep) {
         servo1.write(angle);
-//        Serial.println(angle);
         delay(20);
     }
 }
@@ -62,9 +60,10 @@ void closeGate(){
     }
 }
 
-
 void setup(){
   Serial.begin(115200);
+  pinMode(merah, OUTPUT);
+  pinMode(kuning, OUTPUT);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -104,15 +103,13 @@ void setup(){
 }
 
 void loop(){  
-  //Flash string (PROGMEM and  (FPSTR), String, C/C++ string, const char, char array, string literal are supported
-  //in all Firebase and FirebaseJson functions, unless F() macro is not supported.
   delay(20);
   delayGate = 1;
   if (Firebase.ready())
   { 
-    if(Firebase.getInt(fbdo, "/gate1/jarak")){
-      jarak = fbdo.to<int>();      
-      Serial.printf("jarak firebase: %s\n", String(jarak));
+    if(Firebase.getInt(fbdo, "/gate1/tinggiAir")){
+      tinggiAir = fbdo.to<int>();      
+      Serial.printf("Tinggi Air firebase: %s\n", String(tinggiAir));
     }
     if(Firebase.getString(fbdo, "/gate1/statusGate")){
       statusGate = fbdo.to<String>();
@@ -123,19 +120,21 @@ void loop(){
       Serial.printf("delay firebase: %s\n", String(delayGate));
     }
 
-    if(jarak<=20 and statusGate=="close"){
+    if(tinggiAir<=20 and statusGate=="open"){
       openGate();
-//      delay(delayGate*1000);
-      Serial.printf("Set string... %s\n", Firebase.setString(fbdo, "/gate1/statusGate", "open") ? "ok" : fbdo.errorReason().c_str());
-    }else if(jarak>20 and statusGate=="open"){
+      digitalWrite(merah,LOW);
+      digitalWrite(kuning,HIGH);
+      Serial.printf("Set Status Gate... %s\n", Firebase.setString(fbdo, "/gate1/statusGate", "close") ? "ok" : fbdo.errorReason().c_str());
+    }else if(tinggiAir>20 and statusGate=="close"){
       closeGate();
-//      delay(delayGate*1000);
-      Serial.printf("Set string... %s\n", Firebase.setString(fbdo, "/gate1/statusGate", "close") ? "ok" : fbdo.errorReason().c_str());
+      digitalWrite(merah,HIGH);
+      digitalWrite(kuning,LOW);
+      Serial.printf("Set Status Gate... %s\n", Firebase.setString(fbdo, "/gate1/statusGate", "open") ? "ok" : fbdo.errorReason().c_str());
     }
-    jarakSensor = afstandssensor.afstandCM();
-    Serial.printf("jarak sensor: %s cm \n", String(jarakSensor));
-    Serial.printf("Set jarak... %s\n", Firebase.setInt(fbdo, "/gate1/jarak", jarakSensor) ? "ok" : fbdo.errorReason().c_str());
-    
+    tinggiSensor = afstandssensor.afstandCM();
+    Serial.printf("Tinggi Air: %s cm \n", String(tinggiSensor));
+    Serial.printf("Set Tinggi Air... %s\n", Firebase.setInt(fbdo, "/gate1/tinggiAir", tinggiSensor) ? "ok" : fbdo.errorReason().c_str());
   }
   delay(delayGate*1000);
+  Serial.printf("Set delay time... %s\n", Firebase.setInt(fbdo, "/gate1/delay", 1) ? "ok" : fbdo.errorReason().c_str()); 
 }
